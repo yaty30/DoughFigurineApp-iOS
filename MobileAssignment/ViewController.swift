7143cfdd9ebae6867680c1a58557658abd81ab19
josueyat <josueyat318@gmail.com>
2022-01-01 16:59:48 +0800

    Initial Commit

---
+++ MobileAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobileAssignment
+//
+//  Created by James Yip on 1/1/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
