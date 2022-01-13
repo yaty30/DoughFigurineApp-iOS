fe3be58ce7a1eec71dd0ed66a36fb343aa7c140b
josueyat <josueyat318@gmail.com>
2022-01-13 15:49:08 +0800

    launch screen updated

--- MobileAssignment/AppDelegate.swift
+++ MobileAssignment/AppDelegate.swift
@@ -15,10 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        Thread.sleep(forTimeInterval: 2.0)
         
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
